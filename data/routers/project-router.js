10e3624c146c229f4d34677a9d301811821c11a2
Zooheck <davidscottflack@gmail.com>
2019-02-15 12:12:36 -0600

    Get project by id

--- data/routers/project-router.js
+++ data/routers/project-router.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const ProjectFuncs = require('../helpers/projectModel')
 
-
-
+router.get('/:id', async (req, res) => {
+    const project = await ProjectFuncs.get(req.params.id)
+    try {
+        res.status(200).json({project})
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({message: "error retrieving project"})
+    }
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 
 const actionsRouter = require('./data/routers/action-router')
-
+const projectsRouter = require('./data/routers/project-router')
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 
 server.use('/api/actions', actionsRouter)
-
+server.use('/api/projects', projectsRouter)
 
 module.exports = server;
