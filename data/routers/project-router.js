e49f30c29b7b7a8e0401c7fe9b18def45f73954f
Zooheck <davidscottflack@gmail.com>
2019-02-15 12:54:34 -0600

    Delete project

--- data/routers/project-router.js
+++ data/routers/project-router.js
@@ -42,4 +42,20 @@ router.put('/:id', async (req, res) => {
         res.status(500).json({message: "error editing project"})
     }
 });
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const project = await ProjectFuncs.remove(req.params.id)
+        if (project > 0) {
+            res.status(200).json({message: `Project ${req.params.id} has been deleted`})
+        } else {
+            res.status(404).json({message: "Could not find project"})
+        }
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Error deleting the project'
+        })
+    }
+});
 module.exports = router;
